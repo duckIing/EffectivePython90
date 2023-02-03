a8c4aa74e46f035e1b61fd6d2f51e2ea5d15de9a
duckIing <duckIing@proton.me>
2023-02-03 14:41:54 +0800

    update 前言，添加下载地址

--- 00-前言/前言.py
+++ 00-前言/前言.py
@@ -4,4 +4,8 @@
 1-用符合 python 风格的方式编程；合理发挥 python 特性优势 —— 不滥用
 2-更有效地编写 python 代码；注意不是 python 代码的效率 —— 请用 C/C++
 3-熟练 python 内置模块可以提高 python 代码编写效率 —— 还是要强调编写效率，而非代码执行效率，见第2条
+
+英文版电子书下载：
+https://wwxm.lanzouo.com/ijkFD0miarle
+密码:g5g5
 """
